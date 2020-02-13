f87b3c3fb6106f9057633b4a02a94dd58156946b
rstagi <r.stagi96@gmail.com>
2020-02-13 16:04:14 +0100

    fixed examples Makefiles, added pull option to build script

--- build_docker_javatrace.sh
+++ build_docker_javatrace.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-printf "\n--- UPDATING Extrae SUBMODULE ---\n\n"
-git submodule update --recursive --remote
-cd installation/extrae; git clean -xdf; git checkout javatrace; git pull; cd ../..;
+if [ "$1" = "-pull" ]; then
+	shift
+	branch=$1
+	printf "\n--- UPDATING Extrae SUBMODULE ---\n\n"
+	git submodule update --recursive --remote
+	cd installation/extrae; git clean -xdf; git checkout $branch; git pull; cd ../..;
+fi
 
 printf "\n\n\n--- BUILDING Docker Image with Extrae ---\n"
 printf "If it's the first time, it will take few minutes\n\n"
